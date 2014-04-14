a3369ae6748191b0924605311e53ef8fe4d55bc5
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-04-14 18:56:03 +0200

    Removed an unnecessary call to `git rev-parse`.

--- git/hooks/pre-commit/restrict-dot-only.js
+++ git/hooks/pre-commit/restrict-dot-only.js
@@ -3,17 +3,13 @@
 var assertDotOnly = require('../assert-dot-only')
 var cp = require('child_process')
 
-cp.exec('git rev-parse --verify HEAD', function(err) {
-	var against = err ? '4b825dc642cb6eb9a060e54bf8d69288fbee4904' : 'HEAD'
+cp.exec('git diff --cached --no-color', function(err, diff) {
+	try {
+		assertDotOnly(diff)
+	} catch(e) {
+		console.log(e.message)
+		return process.exit(1)
+	}
 
-	cp.exec('git diff --cached --no-color', function(err, diff) {
-		try {
-			assertDotOnly(diff)
-		} catch(e) {
-			console.log(e.message)
-			return process.exit(1)
-		}
-
-		process.exit(0)
-	})
+	process.exit(0)
 })
--- git/hooks/pre-commit/validate-line-endings.js
+++ git/hooks/pre-commit/validate-line-endings.js
@@ -4,18 +4,14 @@ var enforcedLineEnding = '\r\n'
 
 var cp = require('child_process')
 
-cp.exec('git rev-parse --verify HEAD', function(err) {
-	var against = err ? '4b825dc642cb6eb9a060e54bf8d69288fbee4904' : 'HEAD'
+cp.exec('git diff --cached --no-color', function(err, diff) {
+	var lines = diff.replace(/diff --git(.|\n)*@@.*\n/g, '')
 
-	cp.exec('git diff --cached --no-color', function(err, diff) {
-		var lines = diff.replace(/diff --git(.|\n)*@@.*\n/g, '')
+	if(/^\+.*[^\r]?\n/m.test(lines)) {
+		console.log('You have mixed line-endings. Please normalize to %s.',
+			enforcedLineEnding.replace('\r', '\\r').replace('\n', '\\n'))
+		return process.exit(1)
+	}
 
-		if(/^\+.*[^\r]?\n/m.test(lines)) {
-			console.log('You have mixed line-endings. Please normalize to %s.',
-				enforcedLineEnding.replace('\r', '\\r').replace('\n', '\\n'))
-			return process.exit(1)
-		}
-
-		process.exit(0)
-	})
+	process.exit(0)
 })
