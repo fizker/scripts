5f1c72082dd07b20963aea42584ff271dde2f752
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-04-14 18:53:32 +0200

    Added an update-hook for the dot-only check.

--- git/hooks/assert-dot-only.js
+++ git/hooks/assert-dot-only.js
@@ -23,8 +23,8 @@ module.exports = function(diff) {
 			return '- ' + file.name
 		})
 		throw new Error('\n----\n'+
-			'You are adding some lines containing `describe.only`'+
-			' or `it.only` to the following files:\n'+
+			'You are adding some lines containing `describe.only`\n'+
+			'or `it.only` to the following files:\n'+
 			msg.join('\n')+
 			'\n\nPlease remove them and try again.\n----'
 		)
