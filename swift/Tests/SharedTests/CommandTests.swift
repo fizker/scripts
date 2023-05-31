6136a58dfd90ccfb0a1a586549b2ac6cf73cbb68
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-05-31 11:24:08 +0200

    Added a test for showing the Command fail

---
+++ swift/Tests/SharedTests/CommandTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+import Shared
+
+final class CommandTests: XCTestCase {
+	func test__execute__catLargeFile__entireFileContentIsReturned() async throws {
+		let exp = XCTestExpectation(description: "Loading file")
+		exp.expectedFulfillmentCount = 1
+		Task {
+			let command = Command("/bin/cat")
+			guard let file = Bundle.module.path(forResource: "BigCatTarget", ofType: nil)
+			else {
+				XCTFail("Could not load file")
+				exp.fulfill()
+				return
+			}
+			let result = try await command.execute(arguments: [ file ])
+			exp.fulfill()
+			XCTAssertFalse(try result.stdout.isEmpty)
+			XCTAssertTrue(try result.stderr.isEmpty)
+		}
+
+		await fulfillment(of: [exp], timeout: 1)
+	}
+}
